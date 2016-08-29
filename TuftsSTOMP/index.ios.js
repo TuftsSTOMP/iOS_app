ea92fb655655faa1ca5c8434a2402a9fafff0c2a
Samuel <samheilbron@Samuels-MacBook-Pro-4.local>
2016-08-29 17:15:36 -0400

    Try to get basic app working

--- TuftsSTOMP/index.ios.js
+++ TuftsSTOMP/index.ios.js
@@ -1,53 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-class TuftsSTOMP extends Component {
+var React = require('react');
+var Native = require('react-native');
+var LoginPage = require('./App/LoginPage');
+
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
+  container: {
+    flex: 1
+  }
+});
+
+
+class TuftsSTOMP extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text style={styles.description}>
+          Search for houses to buy!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={styles.description}>
+          Search by place-name, postcode or search near your location.
         </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('TuftsSTOMP', () => TuftsSTOMP);
+React.AppRegistry.registerComponent('TuftsSTOMP', () => TuftsSTOMP);
